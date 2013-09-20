23531f1b425e85117c2f4194976a680fa05fb564
Rajith Vidanaarachchi <rajith.vidanaarachchi@gmail.com>
2013-09-20 12:33:17 +0530

    Rajith_1.2: Database integration for Client

--- src/System/ClientBundle/Controller/AddClientController.php
+++ src/System/ClientBundle/Controller/AddClientController.php
@@ -3,23 +3,50 @@
 namespace System\ClientBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use System\ClientBundle\Entity\Client;
+use Symfony\Component\HttpFoundation\Request;
 
 class AddClientController extends Controller
 {
-    public function addAction()
+    public function addAction(Request $request)
     {
        //return $this->render('SystemClientBundle:Pages:addClient.html.twig');
-        $form = $this->createFormBuilder()
-                ->add('Client_code', 'text')
-                ->add('Name', 'text')
-                ->add('Contact_Person', 'text')
-                ->add('Telephone_number', 'text')
-                ->add('Address', 'text')
+        
+        $message = $request->get('message', null);
+        
+        $client = new Client();
+        
+        $form = $this->createFormBuilder($client)
+                ->add('code', 'text')
+                ->add('name', 'text')
+                ->add('contactPerson', 'text')
+                ->add('tel', 'text')
+                ->add('address', 'text')
                 ->add('create', 'submit')
                 ->add('clear', 'submit')
                 ->getForm();
+        
+        $form->handleRequest($request);
+        
+        
+        
+        if($form->isValid()){
+            
+            $client=$form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($client);
+            $em->flush();
+            
+            $response = array(
+              'message' => "New client added successfully.",
+            );
+            
+            
+            //return $this->forward('SystemClientBundle:AddClient:add', array("message" => "New client added successfully."));
+            return $this->redirect($this->generateUrl('add', $response));
+        }
 
-        return $this->render('SystemProjectBundle:ProjectCreation:createProject.html.twig', array('form' => $form->createView()));
+        return $this->render('SystemClientBundle:Pages:addClient.html.twig', array('form' => $form->createView(), 'message' => $message));
 
     }
 
