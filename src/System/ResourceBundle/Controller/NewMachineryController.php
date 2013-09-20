c663a6649e3f127e28c67801e23612a1706e7e14
Sanka <nuwan.ras@gmail.com>
2013-09-20 14:52:33 +0530

    Sanka_1_9: Converted html for all the pages into php

--- src/System/ResourceBundle/Controller/NewMachineryController.php
+++ src/System/ResourceBundle/Controller/NewMachineryController.php
@@ -8,7 +8,17 @@ class NewMachineryController extends Controller
 {
     public function addAction()
     {
-         return $this->render('SystemResourceBundle:Pages:addNewMachinery.html.twig');
+         //return $this->render('SystemResourceBundle:Pages:addNewMachinery.html.twig');
+        $form = $this->createFormBuilder()
+                ->add('Machinery_Code', 'text')
+                ->add('Name', 'text')
+                ->add('Purchase_Value', 'text')
+                ->add('Hourly_Rate', 'text')
+                ->add('Useful_Life', 'text')
+                ->add('Submit', 'submit')
+                ->add('Clear', 'submit')
+                ->getForm();
+         return $this->render('SystemResourceBundle:Pages:addNewMachinery.html.twig', array('form' => $form->createView()));
     }
 
 }
